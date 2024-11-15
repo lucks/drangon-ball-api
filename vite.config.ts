e020af811f076b169332765441758febf361fda8
Lucks <lucks17@gmail.com>
2024-11-15 08:36:11 -0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,16 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+const port = parseInt(process.env.PORT || '3000');
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    host: '0.0.0.0',
+    port
+  },
+  preview: {
+    host: '0.0.0.0',
+    port
+  },
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
